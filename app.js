314a0157ec2f178a7fb0633f2e6d1c99d8a68764
Kaptain-T <ezighi.antony@gmail.com>
2024-05-30 19:45:39 +0100

    added some more styles and guessing image, validdating numericity is still an issue

--- app.js
+++ app.js
@@ -14,37 +14,53 @@ function displaymessage(message) {
     messages.textContent = message
 }
 play.addEventListener("click", function(){
-    if (active && 1 < userInputField.value && userInputField.value < 100) {
-      console.log(computerNumber)
-      let userInput = userInputField.value;
-      if(userInput > computerNumber && chances > 1){
-        displaymessage("Too high 📈")
-        chances--
+    let userInput = Number(userInputField.value);
+    console.log(Number.isNaN(userInput))
+    if (
+      active &&
+      !userInput == "" &&
+      1 < userInput &&
+      userInput < 100 &&
+      Number.isInteger(userInput)
+    ) {
+      console.log(computerNumber);
+      if (userInput > computerNumber && chances > 1) {
+        displaymessage("Too high 📈");
+        chances--;
         trialsLeft.textContent = chances;
-      } else if (userInput < computerNumber && chances > 1){
-        displaymessage("Too low 📉")
-        chances--
+      } else if (userInput < computerNumber && chances > 1) {
+        displaymessage("Too low 📉");
+        chances--;
         trialsLeft.textContent = chances;
-      } else if(chances = 1){
-        trialsLeft.textContent = 0
-        document.querySelector("body").style.backgroundColor = "red"
-        displaymessage("Game Over. You lose!🚫")
+      } else if ((chances = 1)) {
+        trialsLeft.textContent = 0;
+        document.querySelector("body").style.backgroundColor = "red";
+        displaymessage("You lose!🚫 Cynthia is an olodo🤣");
+        active = false;
+        document.querySelector(
+          ".compDisplay"
+        ).innerHTML = `It was ${computerNumber}`;
+      } else {
+        displaymessage("Correct! ✅");
+        document.querySelector("body").style.backgroundColor = "yellow";
+        document.querySelector(
+          ".compDisplay"
+        ).innerHTML = `It was ${computerNumber}`;
       }
-        else{
-        displaymessage("Correct! ✅")
-      }
-    } else if (userInputField.value >= 100) {
+    } else if (active && userInput >= 100 && Number.isInteger(userInput)) {
       displaymessage("Enter a value below 100");
-    } else if (userInputField.value <= 1) {
+    } else if (active && userInput <= 1 && Number.isInteger(userInput)) {
       displaymessage("Enter a value above 1");
+    } else if (!Number.isInteger(userInput)) {
+      displaymessage("Enter a whole number!");
     }
 })
 
 document.querySelector(".replay").addEventListener("click", function(){
     displaymessage("Play to guess!")
-    document.querySelector("body").style.backgroundColor =
-      "rgba(4, 4, 5, 0.767)";
-      chances = 3
-      trialsLeft.textContent = 3
+    document.querySelector("body").style.backgroundColor = "#009687";
+    computerNumber = Math.trunc(Math.random() * 100 + 1);
+    chances = 3
+    trialsLeft.textContent = 3
 
 })
